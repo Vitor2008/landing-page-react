1dcc95aae4a9cae6fb6f9920fdfa24c304477380
CODE WEB <gabrielronaldo9343@gmail.com>
2025-07-15 17:15:50 -0400

    implementado os componentes About, Campus, Hero, Testimonials

---
+++ src/Components/Testimonials/Testimonials.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Testimonials.css'
+
+const Testimonials = () => {
+  return (
+    <div className='testimonials'>
+      <img src='' alt='' className='next-btn' />
+    </div>
+  )
+}
+
+export default Testimonials
