0e41d191d531eb7bef912ac248747c061b31c4fe
Vitor Almeida <vitor.almeida@redeflex.com.br>
2025-07-21 08:16:16 -0400

    add responsividade

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -16,13 +16,13 @@ const Navbar = () => {
 
   const [mobileMenu, setMobileMenu] = useState(false);
   const toggleMenu = () => {
-
+    mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
   }
 
   return (
     <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
       <img src={logo} alt="Logo" className='logo' />
-      <ul>
+      <ul className={mobileMenu ? '' : 'hide-mobile-menu'}>
         <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
         <li><Link to='program' smooth={true} offset={-260} duration={500}>Program</Link></li>
         <li><Link to='about' smooth={true} offset={-150} duration={500}>About us</Link></li>
